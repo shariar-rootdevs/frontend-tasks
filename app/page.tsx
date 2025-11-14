d99659d76a078ea325632c5860f053fe61f2ca20
shariar-rootdevs <shariar05rootdevs@gmail.com>
2025-11-14 12:50:44 +0600

    changes in task1

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
 export default function page() {
-  return <div>First Page</div>
+  console.log('')
+
+  function shariar() {
+    console.log('This is my name ')
+  }
+
+  function Myname() {
+    console.log('This is my name')
+  }
+
+  if (true) {
+    console.log('This Statement')
+  }
+
+  return <div>First e</div>
 }
