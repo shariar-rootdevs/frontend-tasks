c101ec7c3c97fa34565781ebacda10ec04fbf239
shariar-rootdevs <shariar05rootdevs@gmail.com>
2025-11-16 12:02:30 +0600

    reusable button compoent done

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,10 +1,22 @@
+import clsx from 'clsx'
+
 type ButtonProps = {
   buttonText: string
   variant: 'primary' | 'secondary' | 'danger' | 'outline'
   size?: 'sm' | 'md' | 'lg' | 'xl'
+  disabled?: boolean
+  loading?: boolean
+  fullWidth?: boolean
 }
 
-export default function Button({ buttonText, variant, size = 'md' }: ButtonProps) {
+export default function Button({
+  buttonText,
+  variant,
+  size = 'md',
+  disabled = false,
+  loading = false,
+  fullWidth = false,
+}: ButtonProps) {
   let sizeClasses = ''
 
   switch (size) {
@@ -22,32 +34,34 @@ export default function Button({ buttonText, variant, size = 'md' }: ButtonProps
       break
   }
 
-  let buttonCls = `rounded-md px-4 py-2 bg-blue-400 cursor-pointer ${sizeClasses}`
-  switch (variant) {
-    case 'primary':
-      buttonCls +=
-        ' bg-blue-500 text-white text-lg hover:bg-blue-400 hover:translate-y-0.5 ease duration-500'
-      break
-    case 'secondary':
-      buttonCls +=
-        ' bg-gray-500 text-white text-lg hover:bg-gray-400 hover:translate-y-0.5 ease duration-500'
-      break
-    case 'danger':
-      buttonCls +=
-        ' bg-red-500 text-white text-lg hover:bg-red-400 hover:translate-y-0.5 ease duration-500'
-      break
-    case 'outline':
-      buttonCls +=
-        ' border border-blue-200 text-blue-500 bg-transparent hover:bg-blue-500 hover:text-white hover:translate-y-0.5 ease duration-500'
-      break
-    default:
-      buttonCls +=
-        ' bg-blue-500 text-white text-lg hover:bg-blue-400 hover:translate-y-0.5 ease duration-500'
-      break
-  }
+  const buttonCls = clsx('rounded-md ease duration-500 hover:translate-y-0.5', sizeClasses, {
+    'cursor-not-allowed opacity-50 hover:translate-y-0 hover:bg-none bg-gray-400':
+      disabled || loading,
+    'cursor-pointer bg-blue-500 text-white hover:bg-blue-400':
+      variant === 'primary' && !disabled && !loading,
+    'cursor-pointer bg-gray-500 text-white hover:bg-gray-400':
+      variant === 'secondary' && !disabled && !loading,
+    'cursor-pointer bg-red-500 text-white hover:bg-red-400':
+      variant === 'danger' && !disabled && !loading,
+    'cursor-pointer border border-blue-200 text-blue-500 bg-transparent hover:bg-blue-500 hover:text-white':
+      variant === 'outline' && !disabled && !loading,
+    'w-full': fullWidth,
+  })
+
+  const spinnerColor = variant === 'outline' ? 'border-blue-500' : 'border-white'
+
   return (
     <div>
-      <button className={`${buttonCls}`}>{buttonText}</button>
+      <button disabled={disabled || loading} className={buttonCls} aria-busy={loading}>
+        <div className='flex gap-1 items-center justify-center'>
+          {buttonText}
+          {loading && (
+            <div
+              className={`h-5 w-5 border-2 ${spinnerColor} border-t-transparent border-solid rounded-full animate-spin`}
+            ></div>
+          )}
+        </div>
+      </button>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Page() {
   return (
     <div className='flex justify-center items-center h-screen'>
       <div className='space-y-3'>
-        <Button buttonText='Click' variant='secondary' size='lg' />
-        <Button buttonText='Click' variant='primary' size='lg' />
+        <Button buttonText='Click' variant='secondary' size='lg' disabled={true} />
+        <Button buttonText='Click' variant='primary' size='lg' loading />
         <Button buttonText='Click' variant='danger' size='lg' />
         <Button buttonText='Click' variant='outline' size='lg' />
 
-        {/* <div className='h-[500px] w-[600px] border border-gray-200 p-4'>
-          <Button buttonText='Click' variant='primary' size='lg' />
-        </div> */}
+        <div className='h-[500px] w-[600px] border border-gray-200 p-4'>
+          <Button buttonText='Click' variant='primary' fullWidth />
+        </div>
       </div>
     </div>
   )
