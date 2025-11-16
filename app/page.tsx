ec8290e1e3bf30ebcc89b2c76e0a475c18011dd2
shariar-rootdevs <shariar05rootdevs@gmail.com>
2025-11-16 22:01:34 +0600

    fix : modal componet modified

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,46 @@
 import { useState } from 'react'
 import Modal from './components/Modal'
 
+type PropsType = {
+  setShow: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const Header = ({ setShow }: PropsType) => (
+  <div className='flex justify-between items-center border-b pb-2'>
+    <p className='font-semibold text-lg'>Modal Title</p>
+    <button
+      onClick={() => setShow(false)}
+      className='text-gray-500 hover:text-black font-bold text-md cursor-pointer'
+    >
+      ×
+    </button>
+  </div>
+)
+
+export const Body = () => (
+  <div className='text-gray-700 border-b pb-3'>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aut rem expedita tenetur eius
+      neque atque, quasi excepturi eligendi in nesciunt accusantium.
+    </p>
+  </div>
+)
+
+export const Footer = ({ setShow }: PropsType) => (
+  <div className='flex justify-end gap-3 pt-2'>
+    <button className='px-4 py-2 bg-red-500 text-white font-medium rounded-md min-w-32 cursor-pointer'>
+      Cancel
+    </button>
+
+    <button
+      onClick={() => setShow(false)}
+      className='px-4 py-2 bg-green-500 text-white font-medium rounded-md min-w-32 cursor-pointer'
+    >
+      Confirm
+    </button>
+  </div>
+)
+
 export default function Page() {
   const [show, setShow] = useState<boolean>(false)
   return (
@@ -34,7 +74,25 @@ export default function Page() {
         </button>
       </div>
 
-      {show && <Modal setShow={setShow} />}
+      {show && (
+        <Modal setShow={setShow}>
+          <div
+            onClick={e => e.stopPropagation()}
+            className='bg-white rounded-lg shadow-lg space-y-3 p-5 w-full max-w-lg'
+          >
+            {/* Header */}
+            <Header setShow={setShow} />
+
+            {/* Body */}
+
+            <Body />
+
+            {/* Footer */}
+
+            <Footer setShow={setShow} />
+          </div>
+        </Modal>
+      )}
     </div>
   )
 }
